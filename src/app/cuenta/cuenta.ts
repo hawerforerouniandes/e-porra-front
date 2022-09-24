6f37e244844ffb4653122a7895e396be94488af6
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-09-24 00:29:08 -0500

    Integración con backend Actualización de saldos

--- src/app/cuenta/cuenta.ts
+++ src/app/cuenta/cuenta.ts
@@ -1,6 +1,7 @@
 export class Cuenta {
   id: number;
   tipo: boolean;
+  descripcion: string;
   valor: number;
   numero_tarjeta: string;
   fecha_registro: Date;
@@ -8,12 +9,14 @@ export class Cuenta {
   constructor(
     id: number,
     tipo: boolean,
+    descripcion: string,
     valor: number,
     numero_tarjeta: string,
     fecha_registro: Date
   ) {
       this.id = id,
       this.tipo = tipo,
+      this.descripcion = descripcion,
       this.valor = valor,
       this.numero_tarjeta = numero_tarjeta,
       this.fecha_registro = fecha_registro
