ce3276f8e7133adaa901409765b6c749b97cb533
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-09-25 20:22:56 -0500

    Actualización visualizar ganacia rol apostador

--- src/app/apuesta/apuesta.ts
+++ src/app/apuesta/apuesta.ts
@@ -4,18 +4,21 @@ export class Apuesta {
     id_apostador: number;
     id_competidor: number;
     id_carrera: number;
+    ganancia: number;
 
     constructor(
         id: number,
         valor_apostado: number,
         id_apostador: number,
         id_competidor: number,
-        id_carrera: number
+        id_carrera: number,
+        ganancia: number
     ) {
         this.id = id,
             this.valor_apostado = valor_apostado,
             this.id_apostador = id_apostador,
             this.id_competidor = id_competidor,
             this.id_carrera = id_carrera
+            this.ganancia = ganancia
     }
 }
